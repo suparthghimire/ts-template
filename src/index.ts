f9bf4ecc540e10e4ea35c6b52ab8c26460d117cd
suparthghimire <suparthnarayanghimire2014@gmail.com>
2022-12-01 01:31:10 +0545

    Automation Complete

---
+++ src/index.ts
@@ -0,0 +1,100 @@
+#!usr/bin/env node
+
+import fs from "fs/promises";
+import fsSync from "fs";
+import path from "path";
+import { program } from "commander";
+import prompt from "inquirer";
+
+program.version("0.0.1");
+
+class TemplateService {
+  static async GetTemplates({
+    buildDirName,
+    entryFileName,
+    projectDirName = ".",
+  }: {
+    projectDirName?: string;
+    buildDirName: string;
+    entryFileName: string;
+  }) {
+    const templateDir = path.join(process.cwd(), "templates");
+    const filesNames = await fs.readdir(templateDir);
+    const fileContents = await Promise.all(
+      filesNames.map((fileName) => {
+        const filePath = path.join(templateDir, fileName);
+        return fs.readFile(filePath, "utf-8");
+      })
+    );
+    const strippedNames = filesNames.map((fileName) => {
+      return fileName.replace(".template", "");
+    });
+
+    const savePath = path.join(process.cwd(), projectDirName);
+
+    const pathExists = fsSync.existsSync(savePath);
+
+    if (!pathExists) await fs.mkdir(savePath, { recursive: true });
+    else {
+      const { overwrite } = await prompt.prompt([
+        {
+          type: "confirm",
+          name: "overwrite",
+          message: `Directory ${buildDirName} already exists. Overwrite its Contents?`,
+          default: false,
+        },
+      ]);
+      if (!overwrite) return;
+    }
+
+    for (let i = 0; i < strippedNames.length; i++) {
+      const strippedName = strippedNames[i];
+      let fileContent = fileContents[i];
+      fileContent = fileContent.replace(/{{build__dir__name}}/g, buildDirName);
+      fileContent = fileContent.replace(
+        /{{entry__file__name}}/g,
+        entryFileName
+      );
+      await fs.writeFile(path.join(savePath, strippedName), fileContent);
+    }
+  }
+}
+
+const questions = [
+  {
+    type: "input",
+    name: "projectDirName",
+    message:
+      "Name of the project directory? (Leave blank for current directory)",
+    default: ".",
+  },
+  {
+    type: "input",
+    name: "entryFileName",
+    message: "Name of the entry file? (Leave blank for index.ts)",
+    default: "index",
+  },
+  {
+    type: "input",
+    name: "buildDirName",
+    message: "Name of the build directory? (Leave blank for dist)",
+    default: "dist",
+  },
+];
+
+program
+  .command("init")
+  .alias("i")
+  .description("Initialize TS Project")
+  .action(async () => {
+    prompt.prompt(questions).then(async (answers) => {
+      const { projectDirName, entryFileName, buildDirName } = answers;
+      await TemplateService.GetTemplates({
+        projectDirName,
+        entryFileName,
+        buildDirName,
+      });
+    });
+  });
+
+program.parse(process.argv);
